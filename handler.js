76984811a00fc499995aa1dec7133c830ea4ccab
Gustav Jorlöv <gustav.jorlov@gmail.com>
2018-08-28 22:14:24 +0200

    derp

--- handler.js
+++ handler.js
@@ -1,31 +1,30 @@
 const { graphql, buildSchema } = require('graphql');
 
 // // Construct a schema, using GraphQL schema language
-// var schema = buildSchema(`
-//   type Query {
-//     hello: String
-//   }
-// `);
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
 
 // // The root provides a resolver function for each API endpoint
-// var root = {
-//   hello: () => {
-//     return "Hello world!";
-//   }
-// };
+var root = {
+  hello: () => {
+    return "Hello world!";
+  }
+};
 
 // // Run the GraphQL query '{ hello }' and print out the response
-// graphql(schema, "{ hello }", root).then(response => {
-//   console.log(response);
-// });
+graphql(schema, "{ hello }", root).then(response => {
+  console.log(response);
+  return {de: 'rp'};
+});
 
 module.exports.graphql = async (event, context) => {
-  
+  console.log(event, context);
 };
 
 module.exports.hello = async (event, context) => {
-  const method = event.httpMethod;
-
   return {
     statusCode: 200,
     body: JSON.stringify({
