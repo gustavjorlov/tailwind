94d5975ea0686965912ca5ebce3ccf533fccea3c
Gustav Jorlöv <gustav.jorlov@gmail.com>
2018-08-28 22:06:22 +0200

    cooode

--- handler.js
+++ handler.js
@@ -1,12 +1,37 @@
-'use strict';
+const { graphql, buildSchema } = require('graphql');
+
+// // Construct a schema, using GraphQL schema language
+// var schema = buildSchema(`
+//   type Query {
+//     hello: String
+//   }
+// `);
+
+// // The root provides a resolver function for each API endpoint
+// var root = {
+//   hello: () => {
+//     return "Hello world!";
+//   }
+// };
+
+// // Run the GraphQL query '{ hello }' and print out the response
+// graphql(schema, "{ hello }", root).then(response => {
+//   console.log(response);
+// });
+
+module.exports.graphql = async (event, context) => {
+  
+};
 
 module.exports.hello = async (event, context) => {
+  const method = event.httpMethod;
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message: 'Go Tailwind v1.0! Your function executed successfully!',
+      input: event
+    })
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
